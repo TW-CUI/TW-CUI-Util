e6f29e2be46f44971b32a0fa4c54cccf642b825f
Thomas Ward <teward@thomas-ward.net>
2024-05-26 23:12:35 -0400

    Version bump.
    Major change: Remove MODEL, CLIP, VAE from Generation Parameters, add as its own loader element.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from ._mappings import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'Thomas Ward <teward@thomas-ward.net>'
 __copyright__ = 'Copyright 2024'
 __license__ = "GPL-3.0"
